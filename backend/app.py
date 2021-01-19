b5f565b4180de6850feaad72c4e0cc1a01e10ba5
jacobkaraul <jacob.karaul@gmail.com>
2021-01-19 18:16:27 -0500

    angular and flask containers created

---
+++ backend/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/test')
+def test_endpoint():
+    return 'Goodbye Friend'
