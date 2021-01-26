d3d4b3e8ae544d5405f1e89f4ed100c9b9009d7e
jacobkaraul <jacob.karaul@gmail.com>
2021-01-25 22:52:14 -0500

    deleted sql dir

--- sql/db-init.sql
+++
@@ -1,8 +0,0 @@
---DROP DATABASE IF EXISTS ponics;
-DROP SCHEMA IF EXISTS sensor_data;
-CREATE SCHEMA sensor_data;
-
-CREATE TABLE sensor_data.sensor_template (
-  timestamp timestamp without time zone,
-  sensor_id VARCHAR(255)
-)
