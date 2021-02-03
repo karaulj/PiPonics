d9a669f5e396d7ebd3d1bcfd1b2a07cdc745b8f2
jacobkaraul <jacob.karaul@gmail.com>
2021-02-03 16:42:25 -0500

    started api

--- backend-server-flask/main.py
+++ backend-server-flask/main.py
@@ -1,6 +1,11 @@
+import os
 from flask import (
     Blueprint, request
 )
+
+
+KEY_ID = 'id'
+
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
@@ -10,6 +15,7 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/test')
-def test_endpoint():
-    return 'Goodbye Friend'
+@api.route('/system/all', methods=('GET'))
+def get_all_systems():
+    id = request.args.get(KEY_ID)
+    
