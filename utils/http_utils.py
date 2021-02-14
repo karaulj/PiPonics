4c5b3b9ec445d6e49d86c84ee640bdf0d8c05a1a
jacobkaraul <jacob.karaul@gmail.com>
2021-02-14 15:56:50 -0500

    moved API constants

--- utils/http_utils.py
+++ utils/http_utils.py
@@ -2,11 +2,18 @@
 
 class HTTPHeaders:
     CONTENT_TYPE = "Content-Type"
-    API_UUID = "uuid"
-    API_START_TIME = "startTime"
-    API_END_TIME = "endTime"
+
 
 class HTTPHeaderValues:
     APPLICATION_JSON = "application/json"
     TEXT_PLAIN = "text/plain"
     TEXT_HTML = "text/html"
+
+
+class APIParams:
+    UUID = "uuid"
+    START_TIME = "startTime"
+    END_TIME = "endTime"
+    SENSOR_READING_TIME = "t"
+    SENSOR_READING_VALUE = "v"
+    ACTUATOR_DRIVE_VALUE = "value"
